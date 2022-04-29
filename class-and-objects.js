4475289d457ed79f6ba8f56bc664e6bac7a8a90e
SwaminathanS1215 <swaminathan.shanmugam@wissen.com>
2022-04-29 09:27:20 +0530

    class-objects

--- class-and-objects.js
+++ class-and-objects.js
@@ -22,6 +22,7 @@ var Tshirt = /** @class */ (function () {
     // Display the properties of T-Shirt
     Tshirt.prototype.display = function () {
         var tShirtData = "Name: ".concat(this.name, " \n Brand: ").concat(this.brand, " \n Size: ").concat(this.size, " \n Gender: ").concat(this.gender, " \n Price: ").concat(this.price, " \n Type: ").concat(this.type, " \n Color: ").concat(this.color);
+        
         console.log(tShirtData);
     };
     return Tshirt;
